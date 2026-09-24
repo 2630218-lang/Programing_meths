c9111ad685358d111990bd94a3ade2bb69bed08f
Luisjasso <2630218@upv.edu.mx>
2026-09-23 18:51:22 -0600

    Numeros y error de syntaxyss

---
+++ src/undersating_numbers.py
@@ -0,0 +1,16 @@
+#Numeros
+# Enteros-Intengers
+
+"""
+Los numeros enteros los podemos sumar (+),restar(-),multiplicar(*),
+y dividir 
+"""
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/3)
+
+number_1= 5
+number_2= 10 
+
+print(number_1 + number_2)
---
+++ src/understating_syntaxerror.py
@@ -0,0 +1,22 @@
+
+#eliminacion de espacios en blanco 
+
+programing_lenguaje = "        python          "
+
+print(programing_lenguaje)
+
+print(programing_lenguaje.lstrip())
+print("efecto del lstrip")
+#quita los epacios de la izquierda
+print(programing_lenguaje.rstrip())
+print("efecto del rstrip")
+#quita los espacios de la derecha
+print(programing_lenguaje.strip())
+print("efecto del strip")
+#quita todos los espacios de izquierda y derecha
+print(programing_lenguaje)
+
+#error de syntaxys
+
+message = "una fortaleza de 'python' es su comunidad"
+print(message)
